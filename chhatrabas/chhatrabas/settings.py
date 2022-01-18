b5329a1e370bf5901686594de7fc56c3afdd816e
kaushal289 <khanalkaushal9@gmail.com>
2022-01-18 10:46:54 +0545

    jkhfsd

--- chhatrabas/chhatrabas/settings.py
+++ chhatrabas/chhatrabas/settings.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
-"""
 
+"""
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.messages'
     'django.contrib.staticfiles',
 ]
 
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'chhatrabas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
+        'NAME': 'chhatrabas',
         'HOST': '127.0.0.1' ,
-        'PORT':'3307',
+        'PORT':'3306',
         'USER':'root',
         'PASSWORD':'',
     }
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
