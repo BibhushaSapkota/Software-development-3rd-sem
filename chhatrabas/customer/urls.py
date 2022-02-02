13d38c8fafb40de5a994b0f464012d1214b41147
githubkrijal <krijalshrestha32@gmail.com>
2022-02-02 18:06:33 +0545

    commit

--- chhatrabas/customer/urls.py
+++ chhatrabas/customer/urls.py
@@ -8,14 +8,8 @@ urlpatterns = [
     path('blog',views.blog),
     path('contact',views.contact),
     path('hostel',views.hostel),
-<<<<<<< HEAD
-    path('userprofile',views.userprofile)
-    ]
-
-=======
     path('hostelprofile',views.hostelprofile),
     path('home',views.home),
     path('userprofile',views.userprofile),
     path('signout',views.signout)
 ]
->>>>>>> e2c98b2284994635734c886c8136bccf46b2f9fd
