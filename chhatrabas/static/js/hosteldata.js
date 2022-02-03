9b984ba9e6331686ed6526612654f1c1097abcef
kaushal289 <khanalkaushal9@gmail.com>
2022-02-03 18:17:31 +0545

    commit

--- chhatrabas/static/js/hosteldata.js
+++ chhatrabas/static/js/hosteldata.js
@@ -1,7 +1,7 @@
 var hostels = [
   {
     "id": 1,
-    "name": "Everest hostel",
+    "name": "Daimen Boys Hostel",
     "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
     "reviews2": "best hostel ",
     "reviews3": "nice",
@@ -15,7 +15,7 @@ var hostels = [
     ],
     "Price": 14200,
     "genre": [
-      "Girls only",
+      "Boys only",
       "24 hours electricity",
       "veg food only"
     ],
