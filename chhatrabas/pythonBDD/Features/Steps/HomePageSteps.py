28f2adbe0dd454361a6b7e9d741c2f9af0b7eea5
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-02-04 16:44:11 +0545

    commit

--- chhatrabas/pythonBDD/Features/Steps/HomePageSteps.py
+++ chhatrabas/pythonBDD/Features/Steps/HomePageSteps.py
@@ -1,21 +1,21 @@
 import imp
 from behave import *
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from pythonBDD.Utilities.customLogger import LogGen
 from pythonBDD.Utilities.readproperty import ReadConfig
 
 baseURL=ReadConfig.getURL()
 mylogger=LogGen.loggen()
 
-@given('Launch the browser')
+@given(u'Launch the browser')
 def stepl_impl(context):
-    context.driver=webdriver.Chrome(ChromeDriverManager().install)
+    context.driver=webdriver.Chrome(ChromeDriverManager().install())
     mylogger.info("****Driver Installed*******")
     context.driver.get(baseURL)
-    mylogger.info("Browser lauched")
+    mylogger.info("****Browser lauched****")
 
-@then('verify the page title')
+@then(u'verify the page title')
 def step_imp(context):
     actual_title=context.driver.title
     print(actual_title)
@@ -29,7 +29,7 @@ def step_imp(context):
         mylogger.info("****Title not matched*****")
         assert False
 
-@then('close the browser')
+@then(u'close the browser')
 def step_impl(context):
     context.driver.close()
     mylogger.info('*****Browser closed')
--- chhatrabas/pythonBDD/Features/Steps/LoginPageSteps.py
+++ chhatrabas/pythonBDD/Features/Steps/LoginPageSteps.py
@@ -1,25 +1,48 @@
 from behave import *
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from pythonBDD.Utilities.customLogger import LogGen
 from pythonBDD.Utilities.readproperty import ReadConfig
+from pythonBDD.Pages import HomePage,LoginPage
+import time
 
+baseURL=ReadConfig.getURL()
+mylogger=LogGen.loggen()
 
 @given(u'Launch the app')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Launch the app')
+    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    mylogger.info("****Driver Installed*******")
+    context.driver.get(baseURL)
+    mylogger.info("****Browser lauched****")
+
 
 
 @when(u'enter login credentials')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When enter login credentials')
+    mylogger.info("****Passing Credentials****")
+    global homepage
+    global loginpage
+    homepage=HomePage(context.driver)
+    homepage.clickOnLogin()
+    loginpage= LoginPage()
+    user = ReadConfig.getUserName()
+    pwd = ReadConfig.getPassword()
+    time.sleep(3)
+
+    loginpage.setusername(user)
+    loginpage.setpassword(pwd)
+    mylogger.info("****Entered credential****")
 
 
 @then(u'click login')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then click login')
+    loginpage.clickOnLogin()
+    mylogger.info("****Login Button Clicked****")
+
 
 
 @then(u'close the App')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then close the App')
+    context.driver.close()
+    mylogger.info('*****Browser closed')
