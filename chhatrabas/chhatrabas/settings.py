ae570b15ccdb6b235d82d603664eeb01b30b66b6
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-02-04 19:04:29 +0545

    commit

--- chhatrabas/chhatrabas/settings.py
+++ chhatrabas/chhatrabas/settings.py
@@ -128,4 +128,3 @@ STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"python.autoComplete.extraPaths": ["./path-to-your-code"]
