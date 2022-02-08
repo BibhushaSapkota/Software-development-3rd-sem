87e0a4533a2083f0205c51e58800e36b4c3f3a2f
kaushal289 <khanalkaushal9@gmail.com>
2022-02-08 21:59:30 +0545

    commit

--- chhatrabas/hostel/tests.py
+++ chhatrabas/hostel/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.test import SimpleTestCase
+from django.urls import reverse,resolve
+from hostel.views import *
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_register_url_is_resolved(self):
+        url=reverse('register')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, register)
+
+    def test_login_url_is_resolved(self):
+        url=reverse('login')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, login_redirect)
+
+    def test_dashboard_url_is_resolved(self):
+        url=reverse('dashboard')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, dashboard)
--- chhatrabas/user/tests.py
+++ chhatrabas/user/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.test import SimpleTestCase
+from django.urls import reverse,resolve
+from user.views import *
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_admindash_url_is_resolved(self):
+        url=reverse('admindash')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, admindash)
+
+    def test_edit_url_is_resolved(self):
+        url=reverse('edit')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, edit)
+
+    def test_update_url_is_resolved(self):
+        url=reverse('update/1')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, update/1)
+
+    def test_delete_url_is_resolved(self):
+        url=reverse('delete')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, delete)
