a41316b3d3e67fe99e14c0ad7a1ce8146863891f
kaushal289 <khanalkaushal9@gmail.com>
2022-02-15 17:50:54 +0545

    commit

--- chhatrabas/static/js/hosteldata.js
+++ chhatrabas/static/js/hosteldata.js
@@ -87,51 +87,52 @@ var hostels = [
     "image2": "/static/img/1.jpg",
     "link": "profile.html",
     "email":"everest2020@gmail.com",
-    "contact":9841242424,
+    "contact":9841222222,
     "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
     "star":"★★★★★"
     
   },
   {
     "id": 4,
-    "name": "Daimen Boys Hostel",
+    "name": "Royale hostel",
     "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
     "reviews2": "best hostel ",
     "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
+    "rating": 4,
+    "location": "Kathmandu,Thamel",
+    "roommate": 3,
     "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
+      "Grand school",
+      "royale college",
+      "NCCS engeneering college"
     ],
-    "Price": 14200,
+    "Price": 19200,
     "genre": [
       "Boys only",
       "24 hours electricity",
-      "veg food only"
+      "both veg and non veg",
+      "24 hours water supply"
     ],
     "Status": "Verified",
     "image": "/static/img/1.jpg",
     "image1": "/static/img/2.jpg",
     "image2": "/static/img/1.jpg",
     "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
+    "email":"royale@gmail.com",
+    "contact":984133333,
+    "bio":"Royale hostel This is the best hostel you can find in the perfect location.",
+    "star":"★★★★"
     
   },
   {
     "id": 5,
-    "name": "Daimen Boys Hostel",
+    "name": "Janaki hostel",
     "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
     "reviews2": "best hostel ",
     "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
+    "rating": 3,
+    "location": "Janakpur,Janaki mandir",
+    "roommate": 4,
     "Nearby": [
       "Balaju school",
       "Sunshine college",
@@ -141,60 +142,61 @@ var hostels = [
     "genre": [
       "Boys only",
       "24 hours electricity",
-      "veg food only"
+      "veg food only",
+      "internet"
     ],
     "Status": "Verified",
     "image": "/static/img/1.jpg",
     "image1": "/static/img/2.jpg",
     "image2": "/static/img/1.jpg",
     "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
+    "email":"janakpur2020@gmail.com",
+    "contact":9841444444,
     "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★" 
+    "star":"★★★" 
   },
   {
     "id": 6,
-    "name": "Daimen Boys Hostel",
+    "name": "Pokhara Youth Hostel",
     "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
     "reviews2": "best hostel ",
     "reviews3": "nice",
     "rating": 5,
-    "location": "Kathmandu,Balaju",
+    "location": "Pokhara",
     "roommate": 1,
     "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
+      "pokhara school",
+      "lakeside college",
+      "Nepal engeneering college"
     ],
-    "Price": 14200,
+    "Price": 8000,
     "genre": [
       "Boys only",
       "24 hours electricity",
-      "veg food only"
+      "both veg and non veg"
     ],
     "Status": "Verified",
     "image": "/static/img/1.jpg",
     "image1": "/static/img/2.jpg",
     "image2": "/static/img/1.jpg",
     "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
+    "email":"pokhara2020@gmail.com",
+    "contact":984155555,
     "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
     "star":"★★★★★"
     
   },
   {
     "id": 7,
-    "name": "Daimen Boys Hostel",
+    "name": "yog hostel",
     "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
+    "reviews2": "best hostel",
     "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
+    "rating": 2,
+    "location": "Kathmandu",
+    "roommate": 2,
     "Nearby": [
-      "Balaju school",
+      "Darwin school",
       "Sunshine college",
       "RJS engeneering college"
     ],
@@ -209,14 +211,14 @@ var hostels = [
     "image1": "/static/img/2.jpg",
     "image2": "/static/img/1.jpg",
     "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
+    "email":"yog@gmail.com",
+    "contact":984124444,
+    "bio":"yog hostel This is the best hostel you can find in the perfect location.",
+    "star":"★★"
   },
   {
     "id": 8,
-    "name": "Daimen Boys Hostel",
+    "name": "Holiday Hostel",
     "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
     "reviews2": "best hostel ",
     "reviews3": "nice",
@@ -363,306 +365,5 @@ var hostels = [
     "contact":9841242424,
     "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
     "star":"★★★★★"
-  },
-  {
-    "id": 13,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 14,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 15,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 16,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 17,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 18,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 19,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 20,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 21,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
-  },
-  {
-    "id": 21,
-    "name": "Daimen Boys Hostel",
-    "reviews1": "It was the greatest value of any location where we lived, and we also got to know the best of our fellow travelers. The shared kitchen was an ideal place for assembly! One thing: don’t rely on your GPS to find the mill, because you’re going to be guided (if your GPS is the same as ours) across the river to a spot on the lane. Follow their directions on the website instead.",
-    "reviews2": "best hostel ",
-    "reviews3": "nice",
-    "rating": 5,
-    "location": "Kathmandu,Balaju",
-    "roommate": 1,
-    "Nearby": [
-      "Balaju school",
-      "Sunshine college",
-      "RJS engeneering college"
-    ],
-    "Price": 14200,
-    "genre": [
-      "Boys only",
-      "24 hours electricity",
-      "veg food only"
-    ],
-    "Status": "Verified",
-    "image": "/static/img/1.jpg",
-    "image1": "/static/img/2.jpg",
-    "image2": "/static/img/1.jpg",
-    "link": "profile.html",
-    "email":"everest2020@gmail.com",
-    "contact":9841242424,
-    "bio":"Everest hostel This is the best hostel you can find in the perfect location.",
-    "star":"★★★★★"
   }
-
 ]
