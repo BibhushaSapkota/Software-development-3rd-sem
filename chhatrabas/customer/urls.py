9a58ec115dad3db8a49c3348edf6ab6a9977d0a0
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-02-19 18:37:27 +0545

    commit

--- chhatrabas/customer/urls.py
+++ chhatrabas/customer/urls.py
@@ -2,21 +2,6 @@
 from django.urls import path
 from customer import views 
 urlpatterns = [
-<<<<<<< HEAD
-    path('',views.home),
-    path('register', views.register),
-    path('login',views.login_redirect),
-    path('blog',views.blog),
-    path('contact',views.contact),
-    path('hostel',views.hostel),
-    path('hostelprofile',views.hostelprofile),
-    path('userprofile',views.userprofile),
-    path('signout',views.signout),
-    path('delete/<int:h_id>',views.delete),
-    path('date_update/<int:h_id>', views.date_update),
-    path('billing/<int:h_id>',views.billing),
-    path('billing',views.bill)
-=======
     path('',views.home, name="home"),
     path('register', views.register, name='register'),
     path('login',views.login_redirect,name='login'),
@@ -31,5 +16,4 @@ urlpatterns = [
     path('billing/<int:h_id>',views.billing,name='bill'),
     path('billing',views.bill,name='billing')
 
->>>>>>> 8c5528a018731bc390e1a6b959d8e295df79b42d
 ]
